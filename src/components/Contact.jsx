c33ce7f74f2c5fabc18e003497051c72d4a52779
Nemo080 <nishatf01@gmail.com>
2024-03-12 21:13:22 -0400

    moved imgs to public folder and installed tailwind

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,16 +6,17 @@ const Contact = (props) => {
   return (
       <div>
           <h1>Contact Me</h1>
+          <img src="/random.gif" alt = "gif of a animated girl and cat"/>
           <p>Get in touch with me for any queries or feedback.</p>
           <p>Here is my business phone number: {phone}</p>
           <p>Connect with me on LinkedIn and check out my Github!</p>
 
           <div>
             <a href="https://www.linkedin.com/in/nishat-farhana/">
-              <img className="contact-logos" src="src/assets/pink-linkedin.jpeg" alt= "linkedin logo"/>
+              <img className="contact-logos" src="/pink-linkedin.jpeg" alt= "linkedin logo"/>
             </a>
             <a href="https://github.com/Nemo080">
-              <img className='contact-logos' src="src/assets/github.png" alt= "github logo"/>
+              <img className='contact-logos' src="/github.png" alt= "github logo"/>
             </a>
           </div>
       </div>
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,32 +7,32 @@ export default function Portfolio() {
             <div className='portfolio-item'>
                 <h2>About Me</h2>
                 <p>Just a teeny tiny bit about me. <br/> About Me was programmed using HTML and CSS</p>
-                <img className= "project-img" src="src/assets/AboutMeAssignment.png" alt='about me'/>
+                <img className= "project-img" src="/AboutMeAssignment.png" alt='about me'/>
             </div>
             <div className='portfolio-item'>
                 <h2>Adventure</h2>
                 <p>Wanna have lunch? Well, your options are limited but have a go at this small little lunch adventure and see what you end on. <br /> Programmed with HTML, CSS, JavaScript</p>
-                <img className='project-img' src="src/assets/Adventure.png" alt="lunch adventure"/>
+                <img className='project-img' src="/Adventure.png" alt="lunch adventure"/>
             </div>
             <div className='portfolio-item'>
                 <h2>Rock, Paper, Scissors!</h2>
                 <p>Can you beat a computer at rock, paper, scrssors? Let's see you try! <br /> Fully programmed with JavaScript</p>
-                <img className='project-img' src="src/assets/RPSGame.png" alt= "rock, paper. scissors game" />
+                <img className='project-img' src="/RPSGame.png" alt= "rock, paper. scissors game" />
             </div>
             <div className='portfolio-item'>
                 <h2>Knights of JavaScript</h2>
                 <p>Two-player game... see who can beat the other! <br /> Programmed with HTML, CSS, JavaScript</p>
-                <img className='project-img' src="src/assets/KJSGame.png" alt="knights of javascript" />
+                <img className='project-img' src="/KJSGame.png" alt="knights of javascript" />
             </div>
             <div className='portfolio-item'>
                 <h2>Grocery List</h2>
                 <p>It's just a random list of groceries.... What did you think...? <br /> Programmed with HTML, CSS, JavaScript</p>
-                <img className='project-img' src="src/assets/Grocerylist.png" alt="grocery list"/>
+                <img className='project-img' src="/Grocerylist.png" alt="grocery list"/>
             </div>
             <div className='portfolio-item'>
                 <h2>Anime API</h2>
                 <p>No idea what Spy x Family is? Well, learn about it then... :) <br /> Programmed with HTML, CSS, JavaScript</p>
-                <img className='project-img' src="src/assets/animeAPI.png" alt="spy x family anime api"/>
+                <img className='project-img' src="/animeAPI.png" alt="spy x family anime api"/>
             </div>
         </div>
     </>
