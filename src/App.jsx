609bce00b8d42e803ae9156cf44733b4eac00fe0
Nemo080 <nishatf01@gmail.com>
2024-03-24 01:21:26 -0400

    random edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar.jsx';
 import About from './components/About.jsx';
 import Home from './components/Home.jsx';
 import Contact from './components/Contact.jsx';
@@ -8,23 +9,23 @@ import Contact from './components/Contact.jsx';
 
 
 function App() {
-  const Navbar = () => {
-    return (
-      <nav className="navbar">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-      </nav>
-    )
-  }
+  // const Navbar = () => {
+  //   return (
+  //     <nav className="navbar">
+  //         <ul>
+  //           <li>
+  //             <Link to="/">Home</Link>
+  //           </li>
+  //           <li>
+  //             <Link to="/about">About</Link>
+  //           </li>
+  //           <li>
+  //             <Link to="/contact">Contact</Link>
+  //           </li>
+  //         </ul>
+  //     </nav>
+  //   )
+  // }
 
   return (
     <>
