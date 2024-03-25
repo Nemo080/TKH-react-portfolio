a51ddda1eea951f50874adbb9e8728e711aa9151
Nemo080 <nishatf01@gmail.com>
2024-03-25 12:23:37 -0400

    contact animation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,16 +7,16 @@ const Contact = (props) => {
       <div className='inline-block'>
           <h1 className="text-6xl font-bold my-2.5 mb-10">Contact Me</h1>
           <img className="my-8" src="/random.gif" alt = "gif of a animated girl and cat"/>
-          <p className="text-xl">Get in touch with me for any queries or feedback.</p>
-          <p className="text-xl">Here is my business phone number: {phone}</p>
-          <p className="text-xl">Connect with me on LinkedIn and check out my Github!</p>
+          <p className="text-xl my-2">Get in touch with me for any queries or feedback.</p>
+          <p className="text-xl my-2">Here is my business phone number: {phone}</p>
+          <p className="text-xl my-2">Connect with me on LinkedIn and check out my Github!</p>
 
           <div className='inline-flex'>
             <a href="https://www.linkedin.com/in/nishat-farhana/">
-              <img className="contact-logos" src="/pink-linkedin.jpeg" alt= "linkedin logo"/>
+              <img className="contact-logos animate-pulse" src="/pink-linkedin.jpeg" alt= "linkedin logo"/>
             </a>
             <a href="https://github.com/Nemo080">
-              <img className='contact-logos' src="/github.png" alt= "github logo"/>
+              <img className='contact-logos animate-pulse' src="/github.png" alt= "github logo"/>
             </a>
           </div>
       </div>
